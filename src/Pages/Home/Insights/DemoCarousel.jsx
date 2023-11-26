ae0272c20505d9cc45598c0cb40b09ea19048cd5
rhmunna143 <rhmunna19@gmail.com>
2023-11-26 10:51:00 +0600

    align issue fixed

--- src/Pages/Home/Insights/DemoCarousel.jsx
+++ src/Pages/Home/Insights/DemoCarousel.jsx
@@ -1,7 +1,6 @@
 import { useKeenSlider } from "keen-slider/react"
 import "keen-slider/keen-slider.min.css"
 import "./carousel.css";
-import adImg from "../../../assets/images/ads.jpg"
 import PrimaryBtn from "../../../Components/PrimaryBtn";
 import demoWinner from "../../../assets/images/community-01.png"
 
@@ -46,10 +45,11 @@ const DemoCarousel = () => {
     return (
         <>
             <div ref={sliderRef} className="keen-slider">
-                <div className="keen-slider__slide number-slide1  bg-[080536] text-white slide">
-                    <img src={adImg} alt="ads" />
+                <div className="keen-slider__slide number-slide1 ads-bg text-white py-[335px]">
+                    
                 </div>
-                <div className="keen-slider__slide number-slide2 slide  winner-bg text-white">
+
+                <div className="keen-slider__slide number-slide2 slide winner-bg text-white py-[150px]">
                 
                     <h2 className="text-4xl font-bold">Latest Winner</h2>
                     <div className="img w-64 h-64 rounded-full aspect-square bg-tertiary my-5">
@@ -59,7 +59,8 @@ const DemoCarousel = () => {
                     <h4 className="text-3xl font-bold capitalize">Arnold Denim</h4>
 
                 </div>
-                <div className="keen-slider__slide number-slide3 slide motivation-bg text-white">
+                
+                <div className="keen-slider__slide number-slide3 slide motivation-bg py-[165px] text-white">
                     <h1 className="text-4xl uppercase font-bold my-6 text-center">Become a part of something extraordinary. <br /> Enter the contest!</h1>
                     <div className="rounded-md overflow-hidden shadow-lg bg-gradient-to-r from-blue-500 to-slate-600 p-6 text-white">
                         <h2 className="text-2xl font-bold mb-2">Join our Contest Today!</h2>
