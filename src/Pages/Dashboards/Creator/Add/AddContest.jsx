0112537340f4215e20e58fc2b756bdae28e5b107
rhmunna143 <rhmunna19@gmail.com>
2023-11-28 11:17:21 +0600

    cont post done

--- src/Pages/Dashboards/Creator/Add/AddContest.jsx
+++ src/Pages/Dashboards/Creator/Add/AddContest.jsx
@@ -4,6 +4,24 @@ import { Fragment, useState } from 'react'
 import { Listbox, Transition } from '@headlessui/react';
 import { RiContractUpDownLine } from "react-icons/ri";
 import { FaCheck } from "react-icons/fa";
+import useAllContext from "../../../../Hooks/useAllContext";
+import axios from "axios";
+import { baseURL } from "../../../../Hooks/useAxiosSecure";
+import toast from "react-hot-toast";
+
+/**
+ * {
+    "contestName": "game",
+    "image": "https://i.ibb.co/4F812XJ/mahdi-aminrad-91-KH4-Fun9lc-unsplash.jpg",
+    "description": "Sprite is a lemon-lime flavored soda known for its crisp and refreshing taste. It's caffeine-free and offers a clear, bubbly experience with a zesty citrus twist.",
+    "prize": "44",
+    "prizeMoney": "555",
+    "submissionDetails": "hmm",
+    "deadline": "2023-11-30"
+}
+ */
+
+
 
 const people = [
     { name: 'Select A Tag' },
@@ -17,9 +35,17 @@ const people = [
 const AddContest = () => {
     const [selected, setSelected] = useState(people[0])
 
-    console.log(selected);
-
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const { user, setErr } = useAllContext();
+
+
+
+    const contestCreator = user?.displayName;
+    const creatorUid = user?.uid;
+    const status = "pending";
+    const participants = 0;
+
+
 
     /**
      * TODO: 
@@ -31,10 +57,24 @@ const AddContest = () => {
 
 
     const onSubmit = data => {
-        console.log(data);
 
+        axios.post(`${baseURL}/contests`, {
+            ...data,
+            contestCreator,
+            creatorUid,
+            status,
+            participants
+        })
+            .then(res => {
 
-
+                if (res?.data?._id) {
+                    toast.success("New contest added successfully.")
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                setErr(err)
+            })
     }
 
     return (
@@ -166,7 +206,7 @@ const AddContest = () => {
 
 
 
-
+                    <p className="my-2">Deadline:</p>
                     <input type="date" placeholder="Contest Deadline" {...register("deadline", { required: true })} className="px-4 py-2 bg-forth h-12 text-lg text-white w-full rounded-lg border-[0.5px] border-primary " />
 
                     <br />
