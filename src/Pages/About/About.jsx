e9537a79dc354980180100a15a9cde9a02b8d4f6
rhmunna143 <rhmunna19@gmail.com>
2023-11-30 11:57:29 +0600

    contact page done

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -2,8 +2,28 @@
 
 const About = () => {
     return (
-        <div>
-            about
+        <div className="bg-secondary text-white py-40">
+            <div className="container mx-auto px-4">
+                <div className="flex flex-col lg:flex-row lg:items-center lg:justify-center">
+                    <div className="lg:w-1/2 mr-4">
+                        <img
+                            src="https://i.ibb.co/LJDJ4SB/carl-raw-m3hn2-Kn5-Bns-unsplash.jpg"
+                            alt="Illustration"
+                            className="w-full h-auto"
+                        />
+                    </div>
+                    <div className="lg:w-1/2 py-12">
+                        <h1 className="text-4xl font-bold mb-4">About Us</h1>
+                        <p className="text-lg mb-4">
+                            Welcome to our website! We strive to offer innovative solutions...
+                        </p>
+                        <p className="text-base mb-4">
+                            Our team consists of passionate individuals dedicated to...
+                        </p>
+                        {/* Add more content here */}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -2,8 +2,60 @@
 
 const Contact = () => {
     return (
-        <div>
-            contact
+        <div className="">
+            <div className="bg-secondary text-white py-20">
+                <div className="container mx-auto px-4">
+                    <div className="flex items-center justify-center">
+                        <div className="w-full">
+                            <h1 className="text-4xl font-bold mb-8 text-center">Contact Us</h1>
+                            <form className="max-w-lg mx-auto">
+                                <div className="mb-4">
+                                    <label htmlFor="name" className="block mb-1">
+                                        Name
+                                    </label>
+                                    <input
+                                        type="text"
+                                        id="name"
+                                        name="name"
+                                        placeholder="Enter your name"
+                                        className="w-full px-4 py-2 bg-secondary border border-primary rounded-lg focus:outline-none focus:border-primary"
+                                    />
+                                </div>
+                                <div className="mb-4">
+                                    <label htmlFor="email" className="block mb-1">
+                                        Email
+                                    </label>
+                                    <input
+                                        type="email"
+                                        id="email"
+                                        name="email"
+                                        placeholder="Enter your email"
+                                        className="w-full px-4 py-2 bg-secondary border border-primary rounded-lg focus:outline-none focus:border-primary"
+                                    />
+                                </div>
+                                <div className="mb-4">
+                                    <label htmlFor="message" className="block mb-1">
+                                        Message
+                                    </label>
+                                    <textarea
+                                        id="message"
+                                        name="message"
+                                        rows="5"
+                                        placeholder="Type your message here"
+                                        className="w-full px-4 py-2 bg-secondary border border-primary rounded-lg focus:outline-none focus:border-primary"
+                                    ></textarea>
+                                </div>
+                                <button
+                                    type="submit"
+                                    className="bg-primary text-tertiary py-2 px-6 rounded-lg uppercase font-medium hover:bg-transparent hover:border-primary border border-transparent hover:text-white focus:outline-none"
+                                >
+                                    Send Message
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
